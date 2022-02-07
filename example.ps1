b3dc23add0cd9f9d20cb3782345a8330d57ebe38
GrinsteadDev <93456064+GrinsteadDev@users.noreply.github.com>
2022-02-07 09:29:32 -0500

    Update example.ps1

--- example.ps1
+++ example.ps1
@@ -1,4 +1,4 @@
-. ".\main.ps1"
+. ".\ConvertFrom-NETSCAPEBookmarkFile1.ps1"
 $Bookmarks = Get-Content ".\favorite*" | ConvertFrom-NETSCAPEBookmarkFile1
 
 Write-Host ($Bookmarks | ConvertTo-Json -Dept 5)
